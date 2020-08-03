28d7aefddf2de1ba7570bf76a277f610cdef3eef
vincent wittek# <wittek.vincent@gmail.com>
2020-08-03 17:56:23 +0200

    added bookings model and controller

---
+++ db/migrate/20200803154927_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookings do |t|
+      t.datetime :date
+      t.references :user, null: false, foreign_key: true
+      t.references :lookalike, null: false, foreign_key: true
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
