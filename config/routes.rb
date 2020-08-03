0a3c275f16c5c7acc4e110cda7fa3f1567544478
Felipe Andres Lugo Salazar <felipeandreslugo@gmail.com>
2020-08-03 16:59:20 +0200

    creation of routes for lookalikes and bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,34 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 1. As a visitor I can visit the home page
+  root to: "pages#home"
+
+  # 2. as a user i would like to be able to register
+  # DONE WITH DEVISE
+  # 3. As a user I can create a lookalike
+  # get "/lookalikes/new", to: "lookalikes#new"
+
+  # post "/lookalikes", " lookalikes#create"
+
+  # 4.As a visitor I can browse celeb look-a-likes
+  # get "/lookalikes", to: "lookalikes #index"
+
+  # 5.As a visitor I can view the look-alike I prefer
+  # get "/lookalikes/:id", to: "lookalikes#show"
+
+  # 6.  As a user I can book my look-alike celeb
+  # post "/lookalikes/lookalike_id/bookings", to: "bookings#create"
+
+  # 7. As a user I can access a dashboard
+  # get "/bookings/my_bookins", to: "bookings#my_bookings"
+
+  # =============================================================
+  # this we need to double think
+  # 8. As as user I can rate my experiences
+  # get "/lookalikes/:id/rate", to: "ratings#new"
+  # post "/lookalikes/lookalike_id/bookings", to: "bookings#create"
+
+  resources :lookalikes do 
+    resources :bookings, only: [:create]
+  end
 end
