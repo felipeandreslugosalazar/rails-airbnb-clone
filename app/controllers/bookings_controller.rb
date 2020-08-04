2a9ba6e66b4e2831f002917b9178498e6cd80580
Felipe Andres Lugo Salazar <felipeandreslugo@gmail.com>
2020-08-04 12:12:04 +0200

    def my_bookings added to bookins controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,21 @@
 class BookingsController < ApplicationController
-
   def new
-    @booking = Booking.new  
+    @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.save
+  end
+
+  def my_bookings
+    # this is intended to act as a list index but it
+    # will filter by @user
+
+
+    # here we will have to pass the id of the
+    # in the view i will iterate over @my_bookings to show the user's bookings
+    
   end
 
   private
