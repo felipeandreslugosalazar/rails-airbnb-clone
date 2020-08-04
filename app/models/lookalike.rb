5334ffb3955c4d216146ce71fd5c08d56708d23d
vincent wittek <wittek.vincent@gmail.com>
2020-08-04 11:49:07 +0200

    added validation for descritpion and price, and also inserted € into view of celeb

--- app/models/lookalike.rb
+++ app/models/lookalike.rb
@@ -1,3 +1,6 @@
 class Lookalike < ApplicationRecord
     validates :celeb_name, presence: true
+    validates :price, presence: true, numericality: { only_integer: true }
+    validates :description, presence: true
+
 end
