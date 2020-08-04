ac25dafd0b9074fa19574134ef7c94f17d998efd
Paul Wittchen <wittchenpaul@gmail.com>
2020-08-04 13:41:20 +0200

    ..

--- app/models/lookalike.rb
+++ app/models/lookalike.rb
@@ -2,5 +2,5 @@ class Lookalike < ApplicationRecord
     validates :celeb_name, presence: true
     validates :price, presence: true, numericality: { only_integer: true }
     validates :description, presence: true
-
+    
 end
