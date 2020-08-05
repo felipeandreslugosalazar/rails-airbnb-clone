f4cfad76c052293151a1ea229401ef8297745374
Felipe Andres Lugo Salazar <felipeandreslugo@gmail.com>
2020-08-05 14:03:41 +0200

    schema changed due migration deleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_122620) do
+ActiveRecord::Schema.define(version: 2020_08_04_115536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2020_08_04_122620) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
     t.integer "price"
-    t.string "photo"
     t.index ["user_id"], name: "index_lookalikes_on_user_id"
   end
 
